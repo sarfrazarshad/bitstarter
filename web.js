3514bea19f79149744c46d65ac6c56aa479bb3b7
sarfrazarshad <sarfrazarshad@hotmail.com>
2013-12-05 11:30:41 +0000

    web.js Updated

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  
-response.send(fs.readFileSync('index.html', 'utf8'));
+    
+    var content = fs.readFileSync('index.html', 'utf-8');
+    response.send(content);
 });
 
 var port = process.env.PORT || 5000;
